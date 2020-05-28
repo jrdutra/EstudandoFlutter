0ab5ee9fb58c06c45d687b209234ffcb5ad6e742
jrdutra <jrdutra.com.br@gmail.com>
2020-05-28 11:29:14 -0300

    Primeiro App em Flutter

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -5,20 +5,11 @@ void main(){
     //debugShowCheckedModeBanner: false,
     title: "Frases do dia",
     home: Container(
-      //color: Colors.white,
-      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-      margin: EdgeInsets.all(0),
+      margin: EdgeInsets.only(top: 40),
       decoration: BoxDecoration(
         border: Border.all(width: 3, color: Colors.white)
       ),
-      child: Column(
-        children: <Widget>[
-          Text(
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
-              textAlign: TextAlign.justify,
-          )
-        ],
-      ),
+      child: 
     ),
   ));
 }
