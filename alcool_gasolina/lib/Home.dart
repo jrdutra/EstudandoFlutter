a498f7c4d82a21b9376100b8cc1f4610bc190d2c
jrdutra <jrdutra.com.br@gmail.com>
2020-06-01 19:31:29 -0300

    Entrada de dados

--- alcool_gasolina/lib/Home.dart
+++ alcool_gasolina/lib/Home.dart
@@ -7,6 +7,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  TextEditingController _controllerAlcool = TextEditingController();
+  TextEditingController _controllerGasolina = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,28 +19,73 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.blue,
       ),
       body: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          children: <Widget>[
-            Padding(
-                padding: EdgeInsets.only(bottom: 32),
-                child: Image.asset("images/logo.png")
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Text("Saiba qual a melhor opção para o abastecimento do seu carro."),
-            ),
-            TextField(),
-            TextField(),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: RaisedButton(
-                child: Text("Calcular"),
-                onPressed: (){},
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.only(bottom: 32),
+                  child: Image.asset("images/logo.png")
               ),
-            )
-          ],
-        ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual a melhor opção para o abastecimento do seu carro.",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Alcool, ex: 1.59"
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerAlcool,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Gasolina, ex: 3.59"
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerGasolina,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: RaisedButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(
+                        fontSize: 20
+                    ),
+                  ),
+                  onPressed: (){},
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  "Resultado",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
       ),
     );
   }
