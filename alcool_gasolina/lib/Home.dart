a4d49c53d3cbaf43cd20dce57c0d2684beac7f1f
jrdutra <jrdutra.com.br@gmail.com>
2020-06-01 19:47:45 -0300

    Entrada de dados

--- alcool_gasolina/lib/Home.dart
+++ alcool_gasolina/lib/Home.dart
@@ -10,6 +10,28 @@ class _HomeState extends State<Home> {
 
   TextEditingController _controllerAlcool = TextEditingController();
   TextEditingController _controllerGasolina = TextEditingController();
+  String _textoResultado = "";
+
+  void _calcular(){
+    double precoAlcool = double.tryParse(_controllerAlcool.text);
+    double precoGasolina = double.tryParse(_controllerGasolina.text);
+
+
+    if( precoAlcool == null || precoGasolina == null){
+      setState(() {
+        _textoResultado = "Números inválidos! Digite números maiores que 0 e utilizando (.)";
+      });
+    }else{
+      setState(() {
+        if((precoAlcool/precoGasolina) >= 0.7){
+          _textoResultado = "Melhor abastecer com gasolina!";
+        }else{
+          _textoResultado = "Melhor abastecer com álcool!";
+        }
+      });
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,13 +92,13 @@ class _HomeState extends State<Home> {
                         fontSize: 20
                     ),
                   ),
-                  onPressed: (){},
+                  onPressed: _calcular,
                 ),
               ),
               Padding(
                 padding: EdgeInsets.only(top: 20),
                 child: Text(
-                  "Resultado",
+                  _textoResultado,
                   style: TextStyle(
                       fontSize: 22,
                       fontWeight: FontWeight.bold
