55bf4192627ab1b614eda153179d6d511717fc16
jrdutra <jrdutra.com.br@gmail.com>
2020-06-05 17:10:39 -0300

    Consumo de serviços WEB

---
+++ listas/android/app/src/main/java/com/joaoricardocoredutra/listas/MainActivity.java
@@ -0,0 +1,6 @@
+package com.joaoricardocoredutra.listas;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ preco_bitcoin/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:precobitcoin/Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
