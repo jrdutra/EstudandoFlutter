ceab1983778ce1dfecb2f06ed79f064a411bb644
jrdutra <jrdutra.com.br@gmail.com>
2020-06-05 17:33:17 -0300

    Consumo de serviços WEB

---
+++ listas/lib/Home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List _itens = [];
+
+  void _carregarItens(){
+    _itens = [];
+    for(int i=0; i<10; i++){
+      Map<String, dynamic> item = Map();
+
+      item["titulo"] = "Título ${i} Lorem ipsum dolor sit amet.";
+      item["descricao"] = "Descrição ${i} ipsum dolor sit amet.";
+      _itens.add(item);
+
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    _carregarItens();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Listas"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: ListView.builder(
+            itemCount: _itens.length,
+            itemBuilder: (context, indice){
+              return ListTile(
+                title: Text(_itens[indice]["titulo"]),
+                subtitle: Text(_itens[indice]["descricao"]),
+              );
+            }
+        ),
+      ),
+    );
+  }
+}
