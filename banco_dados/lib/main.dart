3ed7b7db3309d1a2917f539bbca8210872ca93e6
jrdutra <jrdutra.com.br@gmail.com>
2020-06-15 09:05:49 -0300

    Banco de dados

--- banco_dados/lib/main.dart
+++ banco_dados/lib/main.dart
@@ -1,5 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  _recuperarBancoDados() async{
+
+      final caminhoBancoDados = await getDatabasesPath();
+      final localBancoDados = join(caminhoBancoDados, "banco.db");
+
+      var retorno = await openDatabase(
+        localBancoDados,
+        version: 1,
+        onCreate: (db, dbVersaoRecente){
+          String sql = "CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INTEGER)";
+          db.execute(sql);
+        }
+      );
+
+      print("Aberto: " + retorno.isOpen.toString() );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    _recuperarBancoDados();
+
+    return Container();
+  }
 }
