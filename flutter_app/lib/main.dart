5caa70f72546f34a256ae38a6d62667b509115e0
jrdutra <jrdutra.com.br@gmail.com>
2020-06-17 19:36:03 -0300

    Aprenda Inglês - Para Crianças

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -5,5 +5,9 @@ void main() {
   runApp(MaterialApp(
     home: Home(),
     debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primaryColor: Color(0xff795548),
+      scaffoldBackgroundColor: Color(0xfff5e9b9)
+    ),
   ));
 }
--- flutter_app/lib/telas/Home.dart
+++ flutter_app/lib/telas/Home.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp/telas/Bichos.dart';
+import 'package:flutterapp/telas/Numeros.dart';
+import 'package:flutterapp/telas/Vogais.dart';
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
 
   TabController _tabController;
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(
+          length: 3,
+          vsync: this
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         title: Text("Aprenda Inglês"),
         bottom: TabBar(
+          indicatorWeight: 4,
+          indicatorColor: Colors.white,
+          labelColor: Colors.white,
+          labelStyle: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold
+          ),
           controller: _tabController,
           tabs: <Widget>[
             Tab(text: "Bichos"),
@@ -24,8 +52,11 @@ class _HomeState extends State<Home> {
         ),
       ),
       body: TabBarView(
+        controller: _tabController,
         children: <Widget>[
-          
+          Bichos(),
+          Numeros(),
+          Vogais()
         ],
       ),
     );
