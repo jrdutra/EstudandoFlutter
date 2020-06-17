b409dc516a74e6fc17404c43db589b53ee4ce31b
jrdutra <jrdutra.com.br@gmail.com>
2020-06-17 08:04:03 -0300

    Executando sons

--- executando_sons/lib/main.dart
+++ executando_sons/lib/main.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: ,
+    home: Home(),
   ));
+}
+
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  AudioPlayer audioPlayer = AudioPlayer();
+
+  _executar() async{
+
+    String url = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
+    int result = await audioPlayer.play(url);
+    if (result == 1) {
+      // success
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    _executar();
+
+    return Container();
+  }
 }
