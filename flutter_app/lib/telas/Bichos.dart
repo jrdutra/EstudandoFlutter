1b303c08976de20ce9166a537494270cc013fe06
jrdutra <jrdutra.com.br@gmail.com>
2020-06-18 08:16:33 -0300

    Aprenda Inglês - Para Crianças

--- flutter_app/lib/telas/Bichos.dart
+++ flutter_app/lib/telas/Bichos.dart
@@ -8,14 +8,51 @@ class Bichos extends StatefulWidget {
 class _BichosState extends State<Bichos> {
   @override
   Widget build(BuildContext context) {
+
+    double largura = MediaQuery.of(context).size.width;
+    double altura = MediaQuery.of(context).size.height;
+
+
     return GridView.count(
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
       crossAxisCount: 2,
       children: <Widget>[
         GestureDetector(
           onTap: (){
 
           },
-        )
+          child: Image.asset("assets/imagens/cao.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+
+          },
+          child: Image.asset("assets/imagens/gato.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+
+          },
+          child: Image.asset("assets/imagens/leao.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+
+          },
+          child: Image.asset("assets/imagens/macaco.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+
+          },
+          child: Image.asset("assets/imagens/ovelha.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+
+          },
+          child: Image.asset("assets/imagens/vaca.png"),
+        ),
       ],
     );
   }
