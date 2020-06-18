519b5b26aef578b16446dec9226f20be59e5e0a8
jrdutra <jrdutra.com.br@gmail.com>
2020-06-18 08:33:28 -0300

    Aprenda Inglês - Para Crianças

--- flutter_app/lib/telas/Bichos.dart
+++ flutter_app/lib/telas/Bichos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 class Bichos extends StatefulWidget {
   @override
@@ -6,12 +7,27 @@ class Bichos extends StatefulWidget {
 }
 
 class _BichosState extends State<Bichos> {
+
+  AudioCache _audioCache = AudioCache(prefix: "audios/");
+
+  _executar(String nomeAudio){
+
+    _audioCache.play(nomeAudio + ".mp3");
+
+  }
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //Pre carregar arquivos
+    _audioCache.loadAll([
+      "cao.mp3", "gato.mp3"
+    ]);
+  }
+  
   @override
   Widget build(BuildContext context) {
-
-    double largura = MediaQuery.of(context).size.width;
-    double altura = MediaQuery.of(context).size.height;
-
 
     return GridView.count(
       childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
@@ -19,37 +35,37 @@ class _BichosState extends State<Bichos> {
       children: <Widget>[
         GestureDetector(
           onTap: (){
-
+            _executar("cao");
           },
           child: Image.asset("assets/imagens/cao.png"),
         ),
         GestureDetector(
           onTap: (){
-
+            _executar("gato");
           },
           child: Image.asset("assets/imagens/gato.png"),
         ),
         GestureDetector(
           onTap: (){
-
+            _executar("leao");
           },
           child: Image.asset("assets/imagens/leao.png"),
         ),
         GestureDetector(
           onTap: (){
-
+            _executar("macaco");
           },
           child: Image.asset("assets/imagens/macaco.png"),
         ),
         GestureDetector(
           onTap: (){
-
+            _executar("ovelha");
           },
           child: Image.asset("assets/imagens/ovelha.png"),
         ),
         GestureDetector(
           onTap: (){
-
+            _executar("vaca");
           },
           child: Image.asset("assets/imagens/vaca.png"),
         ),
--- flutter_app/lib/telas/Numeros.dart
+++ flutter_app/lib/telas/Numeros.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 class Numeros extends StatefulWidget {
   @override
@@ -6,8 +7,69 @@ class Numeros extends StatefulWidget {
 }
 
 class _NumerosState extends State<Numeros> {
+
+  AudioCache _audioCache = AudioCache(prefix: "audios/");
+
+  _executar(String nomeAudio){
+
+    _audioCache.play(nomeAudio + ".mp3");
+
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //Pre carregar arquivos
+    _audioCache.loadAll([
+      "1.mp3", "2.mp3"
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return GridView.count(
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      crossAxisCount: 2,
+      children: <Widget>[
+        GestureDetector(
+          onTap: (){
+            _executar("1");
+          },
+          child: Image.asset("assets/imagens/1.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("2");
+          },
+          child: Image.asset("assets/imagens/2.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("3");
+          },
+          child: Image.asset("assets/imagens/3.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("4");
+          },
+          child: Image.asset("assets/imagens/4.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("5");
+          },
+          child: Image.asset("assets/imagens/5.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("6");
+          },
+          child: Image.asset("assets/imagens/6.png"),
+        ),
+      ],
+    );
   }
 }
