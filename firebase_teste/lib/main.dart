ad1a04c2958633bfd20e90ab062e1f15220f948f
jrdutra <jrdutra.com.br@gmail.com>
2020-06-19 08:33:09 -0300

    Fire Base - Listen

--- firebase_teste/lib/main.dart
+++ firebase_teste/lib/main.dart
@@ -1,13 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  Firestore.instance
-    .collection("usuarios")
-    .document("pontuacao")
-    .setData({"carlos": "80", "silvana": "340"});
-  
+
+  Firestore db = Firestore.instance;
+  /*
+  db.collection("usuarios")
+    .document("002")
+    .setData(
+      {
+        "nome": "Ana Maria",
+        "idade": "25"
+      }
+  );*/
+  /*
+  DocumentReference ref = await db.collection("noticias")
+  .add(
+      {
+        "Titulo": "Ondas de Calor em São Paulo",
+        "idade": "Texto de exemplo..."
+      }
+  );*/
+
+  //print("Item salvo: "+ ref.documentID);
+  /*
+  db.collection("noticias")
+  .document("9ZAJY2ocDYeefA1Kc7Zt")
+  .setData(
+      {
+        "Titulo": "Ondas de Calor em São Paulo Alterado",
+        "idade": "Texto de exemplo..."
+      }
+  );
+
+  //db.collection("usuarios").document("003").delete();
+
+  DocumentSnapshot snapshot = await db.collection("usuarios").document("001").get();
+  print("dados: " + snapshot.data.toString());
+  */
+
+//  QuerySnapshot querySnapshot = await db.collection("usuarios").getDocuments();
+
+  //print("dados usuarios: " + querySnapshot.documents.toString());
+/*
+  for(DocumentSnapshot item in querySnapshot.documents){
+    var dados = item.data;
+    print("dados usuarios: " + dados.toString());
+  }*/
+
+  db.collection("usuarios")
+      .snapshots()
+      .listen(
+          (snapshot) {
+            for(DocumentSnapshot item in snapshot.documents){
+              var dados = item.data;
+              print("dados usuarios: " + dados.toString());
+            }
+          }
+  );
+
   runApp(App());
 }
 
