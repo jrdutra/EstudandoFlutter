8d2dd86e32a0cbec5b487cbc5eaf4eb3f46cdf3a
jrdutra <jrdutra.com.br@gmail.com>
2020-06-19 16:26:36 -0300

    Fire Base - Auth

--- firebase_teste/lib/main.dart
+++ firebase_teste/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
-  Firestore db = Firestore.instance;
+  //=============================
+  // LENDO E ESCREVENDO NO BANCO
+  //=============================
+  //Firestore db = Firestore.instance;
   /*
   db.collection("usuarios")
     .document("002")
@@ -59,6 +63,7 @@ void main() async{
             }
           }
   );*/
+  /*
   var pesquisa = "an";
   QuerySnapshot querySnapshot = await db.collection("usuarios")
   //.where("nome", isEqualTo: "jamilton").
@@ -73,6 +78,31 @@ void main() async{
   for(DocumentSnapshot item in querySnapshot.documents){
       var dados = item.data;
       print("DADOS: " + dados.toString());
+  }*/
+
+  //======================
+  //LOGIN DE USUARIOS
+  //======================
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+  String email = "jrdutra.com.br@gmail.com";
+  String senha = "Jrdutra1";
+
+  /*auth.createUserWithEmailAndPassword(
+      email: email, 
+      password: senha
+  ).then((firebaseUser) {
+    print("Novo usuario: Sucesso!! e-mail: " + firebaseUser.email);
+  }).catchError((erro){
+    print("Novo usuario: erro: " + erro.toString());
+  });*/
+
+  FirebaseUser usuarioAtual = await auth.currentUser();
+
+  if(usuarioAtual != null){
+      
+  }else{
+
   }
 
   runApp(App());
