b9695ad0d48b65ca8101dfe7561a9e8d3002ce56
jrdutra <jrdutra.com.br@gmail.com>
2020-06-23 10:05:11 -0300

    bitTalk

---
+++ bittalk/lib/Home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          "assets/images/logo.png",
+          height: 40,
+        ),
+        backgroundColor: Colors.black,
+        bottom: PreferredSize(
+          child: Container(
+            color: Color(0xff00f004),
+            height: 2.0,
+          ),
+          preferredSize: Size.fromHeight(2.0),
+        ),
+      ),
+      body: Container(
+        color: Colors.black,
+        child: Center(
+          child: Text(
+            "Conteúdo do APP",
+            style: TextStyle(
+                color: Color(0xff00f004)
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
--- bittalk/lib/main.dart
+++ bittalk/lib/main.dart
@@ -1,48 +1,17 @@
+import 'package:bittalk/Home.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  Firestore.instance
+  .collection("usuarios")
+  .document("001")
+  .setData({"nome":"jamilton"});
+
   runApp(MaterialApp(
     home: Home(),
     debugShowCheckedModeBanner: false,
   ));
 }
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Image.asset(
-            "assets/images/logo.png",
-          height: 40,
-        ),
-        backgroundColor: Colors.black,
-        bottom: PreferredSize(
-          child: Container(
-            color: Color(0xff00f004),
-            height: 2.0,
-          ),
-          preferredSize: Size.fromHeight(2.0),
-        ),
-      ),
-      body: Container(
-        color: Colors.black,
-        child: Center(
-          child: Text(
-              "Conteúdo do APP",
-            style: TextStyle(
-              color: Color(0xff00f004)
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
