b82db488aff7b2ca719c57215362841ee9c77b23
jrdutra <jrdutra.com.br@gmail.com>
2020-06-25 13:04:02 -0300

    bitTalk MeusWidgets

---
+++ bittalk/lib/meusWidgets/GreenCircleAvatar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class GreenCircleAvatar extends StatefulWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+
+  @override
+  _ createState() => _ImagemEfeito();
+
+}
+
+class _GreenCircleAvatar extends State<GreenCircleAvatar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: const Color(0xFFFFE306));
+  }
+}
