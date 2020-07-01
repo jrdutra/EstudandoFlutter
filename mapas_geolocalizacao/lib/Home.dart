939d0b49d527ac387f32600e428236bbb291caac
jrdutra <jrdutra.com.br@gmail.com>
2020-07-01 17:21:05 -0300

    bitTalk Correção de Bugs

--- mapas_geolocalizacao/lib/Home.dart
+++ mapas_geolocalizacao/lib/Home.dart
@@ -10,22 +10,88 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
   Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> _marcadores = {};
+
+  _onMapCreated(GoogleMapController googleMapController){
+    _controller.complete( googleMapController);
+  }
+
+  _movimentarCamera() async{
+
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+            target: LatLng(-22.326128, -41.750137 ),
+            zoom: 18,
+            tilt: 16,
+          bearing: 45
+        ),
+      )
+    );
+  }
+
+  _carregarMarcadores(){
+
+    Set<Marker> marcadoresLocal = {};
+
+    Marker marcadorShopping = Marker(
+      markerId: MarkerId("marcador-shopping"),
+      position: LatLng(-22.335128, -41.749137 ),
+      infoWindow: InfoWindow(
+        title: "Casa"
+      ),
+        icon: BitmapDescriptor.defaultMarkerWithHue(
+          BitmapDescriptor.hueMagenta
+        ),
+      rotation: 45,
+        onTap: (){
+        print("Casa clicada");
+      }
+    );
+    Marker marcadorCartorio = Marker(
+      markerId: MarkerId("marcador-shopping"),
+      position: LatLng(-22.326128, -41.750137 ),
+        infoWindow: InfoWindow(
+            title: "UFRJ"
+        )
+    );
+    marcadoresLocal.add(marcadorShopping);
+    marcadoresLocal.add(marcadorCartorio);
+
+    setState(() {
+      _marcadores = marcadoresLocal;
+    });
+
+  }
+
+  @override
+  void initState() {
+    _carregarMarcadores();
+    super.initState();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Mapas e geolocalização"),),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniStartTop,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.done),
+        onPressed: _movimentarCamera,
+      ),
       body: Container(
         child: GoogleMap(
           mapType: MapType.normal,
+          //mapType: MapType.satellite,
           //-22.335128, -41.749137
           initialCameraPosition: CameraPosition(
             target: LatLng(-22.335128, -41.749137 ),
             zoom: 16
           ),
-          onMapCreated: (GoogleMapController controller){
-            _controller.complete(controller);
-          },
+          onMapCreated: _onMapCreated,
+          markers: _marcadores,
         ),
       ),
     );
