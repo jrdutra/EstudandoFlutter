96ba2ebf8a6b2fbea6c46850014ebd49049b5aa0
jrdutra <jrdutra.com.br@gmail.com>
2020-07-22 16:38:25 -0300

    Xylophone

--- DraAngelaYu/xylophone-flutter-master/lib/main.dart
+++ DraAngelaYu/xylophone-flutter-master/lib/main.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int number){
+    final player = AudioCache();
+    player.play('note$number.wav');
+  }
+
+  Expanded buildKey({int number, Color color}){
+      return Expanded(
+        child: FlatButton(
+          color: color,
+          onPressed: (){
+            playSound(number);
+          },
+        ),
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              buildKey(number: 1, color: Colors.red),
+              buildKey(number: 2, color: Colors.orange),
+              buildKey(number: 3, color: Colors.yellow),
+              buildKey(number: 4, color: Colors.green),
+              buildKey(number: 5, color: Colors.teal),
+              buildKey(number: 6, color: Colors.blue),
+              buildKey(number: 7, color: Colors.purple),
+            ],
+          ),
         ),
       ),
     );
