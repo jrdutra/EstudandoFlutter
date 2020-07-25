3f2499faa7225cb780984444757d96c15efce154
jrdutra <jrdutra.com.br@gmail.com>
2020-07-25 11:07:14 -0300

    BMI Calculator

---
+++ DraAngelaYu/bmi-calculator-flutter-master/lib/icon_content.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+const labelTextStyle = TextStyle(fontSize: 18, color: Color(0xFF8D8E98));
+
+class IconContent extends StatelessWidget {
+
+  IconContent({this.icon, this.text});
+
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          text,
+          style: labelTextStyle,
+        )
+      ],
+    );
+  }
+
+
+}
--- DraAngelaYu/bmi-calculator-flutter-master/lib/input_page.dart
+++ DraAngelaYu/bmi-calculator-flutter-master/lib/input_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'icon_content.dart';
+import 'reusable_card.dart';
+
 const bottonConteinerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
 const bottonConteinerColour = Color(0xFFEB1555);
@@ -14,97 +17,64 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: ReusableCard(
-                    colour: activeCardColour,
-                    cardChild: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          FontAwesomeIcons.mars,
-                          size: 80.0,
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'MALE',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Color(0xFF8D8E98)
-                          ),
-                        )
-                      ],
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColour,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        text: 'MALE',
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: ReusableCard(
-                    colour: activeCardColour,
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColour,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        text: 'FEMALE',
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: ReusableCard(
-              colour: activeCardColour,
+            Expanded(
+              child: ReusableCard(
+                colour: activeCardColour,
+              ),
             ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: ReusableCard(
-                    colour: activeCardColour,
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColour,
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: ReusableCard(
-                    colour: activeCardColour,
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColour,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Container(
-            color: bottonConteinerColour,
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: bottonConteinerHeight,
-          )
-        ],
-      )
-    );
+            Container(
+              color: bottonConteinerColour,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottonConteinerHeight,
+            )
+          ],
+        ));
   }
 }
 
-class ReusableCard extends StatelessWidget {
-
-  ReusableCard({@required this.colour, this.cardChild});
-
-  final Color colour;
-  final Widget cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-          color: colour,
-          borderRadius: BorderRadius.circular(10)
-      ),
-    );
-  }
-
-
-}
